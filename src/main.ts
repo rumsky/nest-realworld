0ec7cc125ede5b6a856d0c326b31e20b0164a85b
rumsky <hongyu.zhao@bitmain.com>
2019-12-17 12:03:08 +0800

    add csurf to mitigate CSFR

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // security CSRF
+  app.use(cookieParser());
+  app.use(csurf({ cookie: true }));
 
   // security: helmet
   app.use(helmet());
